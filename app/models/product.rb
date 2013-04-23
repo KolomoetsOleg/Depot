0f3ede166dfccf161d59b249b3142487d2aa05a4
KolomoetsOleg <KolomoetsOleg@yandex.ru>
2013-04-23 22:17:11 +0300

    Chapter 10 done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
+#!/bin/env ruby
+# encoding: utf-8
 class Product < ActiveRecord::Base
+  has_many :line_items
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description,:image_url, presence: true 
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -6,4 +9,19 @@ class Product < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: {
    with: %r{\.(gif|jpg|png)$}i, message: 'URL must be JPG, PNG or GIF'
   }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
+  private
+
+  #убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
+  end
 end
